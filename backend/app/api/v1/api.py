b617a0eac64d17f18f9cd0d9a7410a59a2a92a3d
amimulamim <amimul.ehsan2001@gmail.com>
2025-06-01 10:15:08 +0600

    api router refactored

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
-from app.api.v1.routes import user, auth
+from app.api.v1.routes import user,auth,chat,quiz
+
 
 api_router = APIRouter()
+
+# Include all API v1 routes
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(user.router, prefix="/user", tags=["User"])
+api_router.include_router(chat.router, prefix="/ai/chat", tags=["Chat"])
+api_router.include_router(quiz.router, prefix="/quiz", tags=["Quiz"])
