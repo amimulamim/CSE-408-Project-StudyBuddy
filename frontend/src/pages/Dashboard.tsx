c9c63c82f6506f77564e5c32e97cd72f5cb2edd9
3m09 <jahinabrar5@gmail.com>
2025-06-05 18:39:34 +0600

    modified the test and ci cd pipelines

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -71,7 +71,7 @@ export default function Dashboard() {
                         variant="outline" 
                         onClick={handleLogout}
                     >
-                        F out
+                        Sign out
                     </Button>
                 </div>
 
