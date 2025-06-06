7a2eede9d8c47fea08147306c336bbf9514849e2
amimulamim <amimul.ehsan2001@gmail.com>
2025-06-06 20:17:15 +0600

    admin code added

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.routes import user,auth,chat,quiz,billing
+from app.api.v1.routes import user,auth,chat,quiz,billing,admin
 
 
 api_router = APIRouter()
@@ -10,3 +10,4 @@ api_router.include_router(user.router, prefix="/user", tags=["User"])
 api_router.include_router(chat.router, prefix="/ai/chat", tags=["Chat"])
 api_router.include_router(quiz.router, prefix="/quiz", tags=["Quiz"])
 api_router.include_router(billing.router, prefix="/billing", tags=["Billing"])
+api_router.include_router(admin.router, prefix="/admin", tags=["Admin"])
